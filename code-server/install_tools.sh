f15323f3816f2dcb073fa37282b8d7b92d46e0d0
Benoît <benoit.pourre@gmail.com>
2023-08-31 13:14:26 +0000

    Resolve "Add Golang, upgrade NodeJS to 20.x"

--- code-server/install_tools.sh
+++ code-server/install_tools.sh
@@ -19,7 +19,7 @@ apt-get update >/dev/null
 apt-get dist-upgrade -y
 apt-get install --no-install-recommends -y vim pwgen jq unzip pass zsh fonts-powerline \
     htop software-properties-common gpg netcat-openbsd uuid-runtime dnsutils exa fd-find skopeo bzip2 \
-    trivy iproute2 nmap iperf3 docker-ce-cli docker-buildx-plugin docker-compose-plugin
+    trivy iproute2 nmap iperf3 docker-ce-cli docker-buildx-plugin docker-compose-plugin golang
 
 echo "Install Ansible and ansible-modules-hashivault"
 # https://www.linuxuprising.com/2023/03/next-debianubuntu-releases-will-likely.html?m=1
@@ -249,7 +249,10 @@ echo "PROMPT='\$(kube_ps1)'\$PROMPT" >> /etc/zsh/zshrc
 echo "export PATH=\$HOME/bin:\$HOME/.local/bin:/usr/local/testssl.sh:\${KREW_ROOT:-\$HOME/.krew}/bin:\$PATH" >> /etc/zsh/zshrc
 
 echo "Install NodeJS and NPM"
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
 apt-get install -y nodejs
 
 echo "Cleaning"
