6f1ee6eb839650c6374baf6415cba57b3924eb62
Benoit Pourre <benoit.pourre@gmail.com>
2024-04-09 10:10:43 +0200

    fix: Fix mongo repo for arm64
    Signed-off-by: Benoit Pourre <benoit.pourre@gmail.com>

--- code-server/install_tools.sh
+++ code-server/install_tools.sh
@@ -13,7 +13,7 @@ echo "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/usr/share/keyrings/t
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
 chmod a+r /usr/share/keyrings/docker.gpg
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 # shellcheck source=/dev/null
 echo \
   "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian \
